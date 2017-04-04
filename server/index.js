82c4c7b5a03ee7864d1a6dcbfbbf542deb1935d8
jelly <zhengguang1211@gmail.com>
2017-04-04 19:33:07 +0800

    add gzip

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 const path = require('path')
+const zlib = require('zlib')
+
 const Koa = require('koa')
 const Router = require('koa-router')
 const bodyParser = require('koa-bodyparser')
 const serve = require('koa-static')
+const compress = require('koa-compress')
 const { graphqlKoa, graphiqlKoa } = require('graphql-server-koa')
 
 const { schema } = require('./src/schema')
@@ -12,9 +15,12 @@ const PORT = process.env.PORT || 4000
 const app = new Koa()
 const router = new Router()
 
+app.use(compress({
+  flush: zlib.Z_SYNC_FLUSH,
+}))
+
 // Koa only serves static assets in production
 if (process.env.NODE_ENV === 'production') {
-  console.log('static files is in ../client/build')
   app.use(serve(path.join(__dirname, '../client/build')))
 }
 
